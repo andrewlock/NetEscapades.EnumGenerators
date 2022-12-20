0b9b294e39d7c4ab55a57181ee0ca879ec5b41d2
Andrew Lock <andrewlock.net@gmail.com>
2022-12-20 21:39:02 +0000

    Fix xml comments (#49)
    * Fix xml docs
    
    * Add a sample project with <GenerateDocumentationFile> so we're alerted about xml docs errors

---
+++ samples/ToStringFastExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NetEscapades.EnumGenerators;
+
+var value = ExampleEnums.First;
+Console.WriteLine(value.ToStringFast());
+
+var flags = FlagsEnum.Flag1 | FlagsEnum.Flag3;
+
+Console.WriteLine(flags.ToStringFast());
+Console.WriteLine($"HasFlag(Flag1), {flags.HasFlagFast(FlagsEnum.Flag1)}");
+Console.WriteLine($"HasFlag(Flag1), {flags.HasFlagFast(FlagsEnum.Flag2)}");
+
+[EnumExtensions]
+internal enum ExampleEnums
+{
+    First,
+    Second,
+    Third,
+}
+
+[EnumExtensions]
+[Flags]
+internal enum FlagsEnum
+{
+    Flag1,
+    Flag2,
+    Flag3,
+}
