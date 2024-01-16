2fe842a55c542061af034d0877947b7e1c0081bd
Andrew Lock <andrewlock.net@gmail.com>
2024-01-16 22:13:50 +0000

    Trying it out

---
+++ src/NetEscapades.EnumGenerators/TrackingNames.cs
@@ -0,0 +1,11 @@
+namespace NetEscapades.EnumGenerators;
+
+/// <summary>
+/// Names that are attached to incremental generator stages for tracking
+/// </summary>
+public static class TrackingNames
+{
+    public const string InitialExtraction = nameof(InitialExtraction);
+    public const string RemovingNulls = nameof(RemovingNulls);
+
+}
--- tests/NetEscapades.EnumGenerators.Tests/TestHelpers.cs
+++ tests/NetEscapades.EnumGenerators.Tests/TestHelpers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Immutable;
 using System.Linq;
+using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 
@@ -8,13 +9,20 @@ namespace NetEscapades.EnumGenerators.Tests;
 
 internal class TestHelpers
 {
+    private static readonly string[] _trackingNames = typeof(TrackingNames)
+        .GetFields()
+        .Where(fi => fi.IsLiteral && !fi.IsInitOnly && fi.FieldType == typeof(string))
+        .Select(x => (string?)x.GetRawConstantValue())
+        .Where(x => !string.IsNullOrEmpty(x))
+        .ToArray()!;
+
     public static (ImmutableArray<Diagnostic> Diagnostics, string Output) GetGeneratedOutput<T>(string source)
         where T : IIncrementalGenerator, new()
     {
         var syntaxTree = CSharpSyntaxTree.ParseText(source);
         var references = AppDomain.CurrentDomain.GetAssemblies()
-            .Where(_ => !_.IsDynamic && !string.IsNullOrWhiteSpace(_.Location))
-            .Select(_ => MetadataReference.CreateFromFile(_.Location))
+            .Where(assembly => !assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
+            .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
             .Concat(new[]
             {
                 MetadataReference.CreateFromFile(typeof(T).Assembly.Location),
@@ -28,14 +36,63 @@ internal class TestHelpers
             references,
             new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
-        var originalTreeCount = compilation.SyntaxTrees.Length;
-        var generator = new T();
+        var runResult = RunGeneratorAndAssertOutput<T>(compilation, _trackingNames);
+        
+        var trees = runResult.GeneratedTrees;
+        return (runResult.Diagnostics, trees.IsDefaultOrEmpty ? string.Empty : trees[trees.Length - 1].ToString());
+    }
 
-        var driver = CSharpGeneratorDriver.Create(generator);
-        driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+    private static GeneratorDriverRunResult RunGeneratorAndAssertOutput<T>(CSharpCompilation compilation, params string[] trackingNames)
+        where T : IIncrementalGenerator, new()
+    {
+        var generator = new T().AsSourceGenerator();
 
-        var trees = outputCompilation.SyntaxTrees.ToList();
+        var opts = new GeneratorDriverOptions(
+            disabledOutputs: IncrementalGeneratorOutputKind.None,
+            trackIncrementalGeneratorSteps: true);
 
-        return (diagnostics, trees.Count != originalTreeCount ? trees[trees.Count - 1].ToString() : string.Empty);
+        var driver = CSharpGeneratorDriver.Create([generator], driverOptions: opts);
+
+        // Run twice, once with a clone
+        var runResult = driver.RunGenerators(compilation).GetRunResult();
+        var compilationClone = compilation.Clone();
+        var runResult2 = driver.RunGenerators(compilationClone).GetRunResult();
+
+        AssertRunsEqual(runResult, runResult2, trackingNames);
+
+        return runResult;
+    }
+
+    private static void AssertRunsEqual(GeneratorDriverRunResult runResult1, GeneratorDriverRunResult runResult2, string[] trackingNames)
+    {
+        var trackedSteps1 = runResult1.Results[0].TrackedSteps;
+        var trackedSteps2 = runResult2.Results[0].TrackedSteps;
+
+        // these should be the same!
+        trackedSteps1.Should()
+            .NotBeEmpty()
+            .And.HaveSameCount(trackedSteps2);
+
+        foreach (var trackingName in trackingNames)
+        {
+            AssertEqual(trackedSteps1, trackedSteps2, trackingName);
+        }
+    }
+
+    private static void AssertEqual(
+        ImmutableDictionary<string, ImmutableArray<IncrementalGeneratorRunStep>> trackedSteps1,
+        ImmutableDictionary<string, ImmutableArray<IncrementalGeneratorRunStep>> trackedSteps2,
+        string stepName)
+    {
+        ImmutableArray<IncrementalGeneratorRunStep> runSteps1 = trackedSteps1.Should().ContainKey(stepName).WhoseValue;
+        ImmutableArray<IncrementalGeneratorRunStep> runSteps2 = trackedSteps2.Should().ContainKey(stepName).WhoseValue;
+
+        for (var i = 0; i < runSteps1.Length; i++)
+        {
+            ImmutableArray<(object Value, IncrementalStepRunReason Reason)> outputs1 = runSteps1[i].Outputs;
+            ImmutableArray<(object Value, IncrementalStepRunReason Reason)> outputs2 = runSteps2[i].Outputs;
+
+            outputs1.Should().Equal(outputs2, $"because {stepName} should produce cacheable outputs");
+        }
     }
 }
