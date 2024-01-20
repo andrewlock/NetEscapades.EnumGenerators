74082d614df6d6597563169b1c367435fa6a5664
Andrew Lock <andrewlock.net@gmail.com>
2024-01-20 23:31:36 +0000

    Making some tweaks

--- src/NetEscapades.EnumGenerators/TrackingNames.cs
+++ src/NetEscapades.EnumGenerators/TrackingNames.cs
@@ -3,7 +3,7 @@ namespace NetEscapades.EnumGenerators;
 /// <summary>
 /// Names that are attached to incremental generator stages for tracking
 /// </summary>
-public static class TrackingNames
+public class TrackingNames
 {
     public const string InitialExtraction = nameof(InitialExtraction);
     public const string RemovingNulls = nameof(RemovingNulls);
