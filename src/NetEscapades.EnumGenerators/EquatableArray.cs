8e2be55669d3cfeef7f122a59a8f084c7c970dd2
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2024-07-04 11:43:19 +0200

    Fix EquatableArray.Equals override to avoid an endless recursion and stackoverflow exception

--- src/NetEscapades.EnumGenerators/EquatableArray.cs
+++ src/NetEscapades.EnumGenerators/EquatableArray.cs
@@ -34,7 +34,7 @@ public readonly struct EquatableArray<T> : IEquatable<EquatableArray<T>>, IEnume
     /// <sinheritdoc/>
     public override bool Equals(object? obj)
     {
-        return obj is EquatableArray<T> array && Equals(this, array);
+        return obj is EquatableArray<T> array && Equals(array);
     }
 
     /// <sinheritdoc/>
