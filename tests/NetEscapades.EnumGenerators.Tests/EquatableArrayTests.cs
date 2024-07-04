6099a6e9fd2f512a33977db887d25c4c2962ac96
Stefan Ossendorf <stefan.ossendorf@googlemail.com>
2024-07-04 11:43:19 +0200

    Fix EquatableArray.Equals override to avoid an endless recursion and stackoverflow exception

---
+++ tests/NetEscapades.EnumGenerators.Tests/EquatableArrayTests.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace NetEscapades.EnumGenerators.Tests;
+public class EquatableArrayTests {
+    [Fact]
+    public void ObjectEqualsWorksAsExpected() {
+        var instances = new EquatableArray<string>(["A"]);
+        object comparand = new EquatableArray<string>(["A"]);
+
+        instances.Equals(comparand).Should().BeTrue();
+    }
+}
