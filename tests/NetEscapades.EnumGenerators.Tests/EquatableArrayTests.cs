4a595b0eb4723fd4ff9f39562bee463943dc1c7f
Andrew Lock <andrewlock.net@gmail.com>
2024-09-30 22:25:15 +0100

    Add some more tests for equatable arrays (#92)
    * Handle potentially multiple sources added by the generator
    
    * Add test for equatable array comparison
    
    * Fix equatable array implementation
    
    In some cases, this would give stack overflow from recursion

--- tests/NetEscapades.EnumGenerators.Tests/EquatableArrayTests.cs
+++ tests/NetEscapades.EnumGenerators.Tests/EquatableArrayTests.cs
@@ -1,13 +1,66 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 using Xunit;
 
 namespace NetEscapades.EnumGenerators.Tests;
-public class EquatableArrayTests {
-    [Fact]
-    public void ObjectEqualsWorksAsExpected() {
-        var instances = new EquatableArray<string>(["A"]);
-        object comparand = new EquatableArray<string>(["A"]);
 
-        instances.Equals(comparand).Should().BeTrue();
+public class EquatableArrayTests
+{
+    [Fact]
+    public void PrimitiveComparison()
+    {
+        int[] val1 = [1, 2, 3, 4, 5];
+        int[] val2 = [1, 2, 3, 4, 5];
+
+        var arr1 = new EquatableArray<int>(val1);
+        var arr2 = new EquatableArray<int>(val2);
+
+        arr1.Equals(arr2).Should().BeTrue();
+    }
+
+    [Fact]
+    public void RecordComparison()
+    {
+        Record[] val1 = [new(1), new(2), new(3), new(4), new(5)];
+        Record[] val2 = [new(1), new(2), new(3), new(4), new(5)];
+
+        var arr1 = new EquatableArray<Record>(val1);
+        var arr2 = new EquatableArray<Record>(val2);
+
+        arr1.Equals(arr2).Should().BeTrue();
+    }
+
+    [Fact]
+    public void NestedEquatableArrayComparison()
+    {
+        EquatableArray<int>[] val1 = [new([1]), new([2]), new([3]), new([4]), new([5])];
+        EquatableArray<int>[] val2 = [new([1]), new([2]), new([3]), new([4]), new([5])];
+
+        var arr1 = new EquatableArray<EquatableArray<int>>(val1);
+        var arr2 = new EquatableArray<EquatableArray<int>>(val2);
+
+        arr1.Equals(arr2).Should().BeTrue();
+    }
+
+    [Fact]
+    public void BoxedNestedEquatableArrayComparison()
+    {
+        EquatableArray<int>[] val1 = [new([1]), new([2]), new([3]), new([4]), new([5])];
+        EquatableArray<int>[] val2 = [new([1]), new([2]), new([3]), new([4]), new([5])];
+
+        object arr1 = new EquatableArray<EquatableArray<int>>(val1);
+        var arr2 = new EquatableArray<EquatableArray<int>>(val2);
+
+        arr1.Equals(arr2).Should().BeTrue();
+        arr2.Equals(arr1).Should().BeTrue();
+    }
+
+    public record Record
+    {
+        public Record(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
     }
 }
