4a3248ddaca27190459862cc0de76c9eddd52be7
Andrew Lock <andrewlock.net@gmail.com>
2024-10-23 22:09:34 +0100

    Bump to 1.0.0-beta11

--- src/NetEscapades.EnumGenerators/Constants.cs
+++ src/NetEscapades.EnumGenerators/Constants.cs
@@ -2,7 +2,7 @@ namespace NetEscapades.EnumGenerators;
 
 public static class Constants
 {
-    public const string Version = "1.0.0-beta10";
+    public const string Version = "1.0.0-beta11";
 
     public const string EnabledPropertyName = "EnableEnumGeneratorInterceptor";
 }
