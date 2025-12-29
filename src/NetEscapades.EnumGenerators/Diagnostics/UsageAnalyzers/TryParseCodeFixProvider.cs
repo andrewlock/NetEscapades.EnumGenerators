9d9c8f374cb0ef395013cf856b6bc7dcb16e6cae
Andrew Lock <andrewlock.net@gmail.com>
2025-12-29 19:43:28 +0000

    Revert "Fix whitespace issue by using WithTriviaFrom in ReplaceNode callback"
    This reverts commit df3d0fedcae8c8192da6fadeff4e7c7d558be9e4.

--- src/NetEscapades.EnumGenerators/Diagnostics/UsageAnalyzers/TryParseCodeFixProvider.cs
+++ src/NetEscapades.EnumGenerators/Diagnostics/UsageAnalyzers/TryParseCodeFixProvider.cs
@@ -3,7 +3,6 @@ using System.Composition;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.Simplification;
@@ -52,84 +51,18 @@ public class TryParseCodeFixProvider : CodeFixProviderBase
             return Task.CompletedTask;
         }
 
-        ArgumentSyntax? valueArgument = null;
-        ArgumentSyntax? outArgument = null;
-        ArgumentSyntax? ignoreCaseArgument = null;
-
-        // Determine which arguments to use
-        if (methodSymbol is { IsGenericMethod: true, TypeArguments.Length: 1 })
-        {
-            // Pattern: Enum.TryParse<TEnum>(value, out result) or Enum.TryParse<TEnum>(value, ignoreCase, out result)
-            if (invocation.ArgumentList.Arguments.Count >= 2)
-            {
-                valueArgument = invocation.ArgumentList.Arguments[0];
-                
-                if (invocation.ArgumentList.Arguments.Count == 2)
-                {
-                    // TryParse<TEnum>(value, out result)
-                    outArgument = invocation.ArgumentList.Arguments[1];
-                }
-                else if (invocation.ArgumentList.Arguments.Count == 3)
-                {
-                    // TryParse<TEnum>(value, ignoreCase, out result)
-                    ignoreCaseArgument = invocation.ArgumentList.Arguments[1];
-                    outArgument = invocation.ArgumentList.Arguments[2];
-                }
-            }
-        }
-        else if (methodSymbol.Parameters.Length is 3 or 4
-                 && invocation.ArgumentList.Arguments.Count >= 3)
-        {
-            // Pattern: Enum.TryParse(typeof(TEnum), value, out result) or Enum.TryParse(typeof(TEnum), value, ignoreCase, out result)
-            valueArgument = invocation.ArgumentList.Arguments[1];
-            
-            if (invocation.ArgumentList.Arguments.Count == 3)
-            {
-                // TryParse(typeof(TEnum), value, out result)
-                outArgument = invocation.ArgumentList.Arguments[2];
-            }
-            else if (invocation.ArgumentList.Arguments.Count == 4)
-            {
-                // TryParse(typeof(TEnum), value, ignoreCase, out result)
-                ignoreCaseArgument = invocation.ArgumentList.Arguments[2];
-                outArgument = invocation.ArgumentList.Arguments[3];
-            }
-        }
-
-        if (valueArgument is null || outArgument is null)
-        {
-            return Task.CompletedTask;
-        }
+        // assume that we've already met all the requirements, so just make the fix
+        var skipFirstArg = methodSymbol is not { IsGenericMethod: true, TypeArguments.Length: 1 };
 
         // Create new invocation: ExtensionsClass.TryParse(value, out result) or ExtensionsClass.TryParse(value, out result, ignoreCase)
         var generator = editor.Generator;
-        
-        editor.ReplaceNode(invocation, (node, _) =>
-        {
-            var inv = (InvocationExpressionSyntax)node;
-            SyntaxNode newInvocation;
-            if (ignoreCaseArgument is not null)
-            {
-                // Call with ignoreCase parameter
-                newInvocation = generator.InvocationExpression(
-                        generator.MemberAccessExpression(generator.TypeExpression(extensionTypeSymbol), "TryParse"),
-                        valueArgument,
-                        outArgument,
-                        ignoreCaseArgument)
-                    .WithAdditionalAnnotations(Simplifier.AddImportsAnnotation, Simplifier.Annotation);
-            }
-            else
-            {
-                // Call without ignoreCase parameter
-                newInvocation = generator.InvocationExpression(
-                        generator.MemberAccessExpression(generator.TypeExpression(extensionTypeSymbol), "TryParse"),
-                        valueArgument,
-                        outArgument)
-                    .WithAdditionalAnnotations(Simplifier.AddImportsAnnotation, Simplifier.Annotation);
-            }
-            return newInvocation.WithTriviaFrom(inv);
-        });
-        
+        var newInvocation = generator.InvocationExpression(
+                generator.MemberAccessExpression(generator.TypeExpression(extensionTypeSymbol), "TryParse"),
+                skipFirstArg ? invocation.ArgumentList.Arguments.Skip(1) : invocation.ArgumentList.Arguments)
+            .WithTriviaFrom(invocation)
+            .WithAdditionalAnnotations(Simplifier.AddImportsAnnotation, Simplifier.Annotation);
+
+        editor.ReplaceNode(invocation, newInvocation);
         return Task.CompletedTask;
     }
 }
